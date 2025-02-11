3cb78b6d6de2558dc8126e331d2958e154467fb1
René Widera <r.widera@hzdr.de>
2025-02-11 15:38:35 +0100

    working changes and generic interface

--- include/alpaka/api/trait.hpp
+++ include/alpaka/api/trait.hpp
@@ -39,7 +39,7 @@ namespace alpaka
             template<typename T_Type, typename T_Api>
             struct Op
             {
-                consteval uint32_t operator()(T_Api const) const
+                constexpr uint32_t operator()(T_Api const) const
                 {
                     static_assert(sizeof(T_Api) && false, "Missing definition of GetArchSimdWidth for API.");
                     return 1u;
@@ -67,7 +67,7 @@ namespace alpaka
             template<typename T_Api>
             struct Op
             {
-                consteval uint32_t operator()(T_Api const) const
+                constexpr uint32_t operator()(T_Api const) const
                 {
                     static_assert(sizeof(T_Api) && false, "GetCachelineSize for the current used API is not defined.");
                     return 42u;
@@ -82,7 +82,7 @@ namespace alpaka
      * @return number of elements that can be processed in parallel in a vector register
      */
     template<typename T_Type>
-    consteval uint32_t getArchSimdWidth(auto const api)
+    constexpr uint32_t getArchSimdWidth(auto const api)
     {
         return trait::GetArchSimdWidth::Op<T_Type, ALPAKA_TYPEOF(api)>{}(api);
     }
@@ -113,7 +113,7 @@ namespace alpaka
      *
      * @return cacheline size in bytes
      */
-    consteval uint32_t getCachelineSize(auto const api)
+    constexpr uint32_t getCachelineSize(auto const api)
     {
         return trait::GetCachelineSize::Op<ALPAKA_TYPEOF(api)>{}(api);
     }
